f76a3b3fdc6cc8355eb430f12f8720a90e704438
Roman Jarmukhametov <roman.jarmukhametov@gmail.com>
2024-04-12 16:29:52 +0500

    Completes destination page

--- app/destination/page.tsx
+++ app/destination/page.tsx
@@ -1,5 +1,15 @@
+import TheSubheading from "@/app/ui/shared/TheSubheading";
+import DestinationTab from "@/app/ui/destination/DestinationTab";
 const Destination = () => {
-  return <div>Destination page</div>;
+  return (
+    <main className="container mx-auto px-4 py-8 sm:py-12 md:py-16 lg:py-20">
+      <TheSubheading level="1">
+        <span className="font-bold opacity-25 mr-3">01</span>
+        Pick your destination
+      </TheSubheading>
+      <DestinationTab />
+    </main>
+  );
 };
 
 export default Destination;
